39e7d7354a39789d1d0afa3e2fe4f583caf03b83
Hung <trh.bkdn@gmail.com>
2023-05-18 20:34:28 +0700

    1

---
+++ buoi3.dart
@@ -0,0 +1,26 @@
+import 'dart:math';
+
+void main() {
+  final numbers = List.generate(10, (index) {
+    final random = Random();
+    return random.nextInt(100);
+  });
+  print(numbers);
+
+  int maxNumber = numbers[0];
+  int minNumber = numbers[0];
+
+  for (int number in numbers) {
+    if (maxNumber <= number) {
+      maxNumber = number;
+    }
+    if (minNumber >= number) {
+      minNumber = number;
+    }
+  }
+
+  print('minNumber: $minNumber');
+  print('maxNumber: $maxNumber');
+
+// code o day
+}
