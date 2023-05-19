7a1b3150b05e2ecb12661cbca83d88b15b3ba1fd
Hung Pham <trh.bkdn@gmail.com>
2023-05-19 10:10:24 +0700

    add practice number 2

--- buoi3.dart
+++ buoi3.dart
@@ -1,64 +1,36 @@
 import 'dart:math';
 
+import 'repo.dart';
+
 void main() {
-  // Generate a list of 10 random numbers.
+  //Buoi 4 - Bai 1
+
+  print('\n-------------------Buoi 4 - Bai 1-----------------------');
+
+  timerPeriodic(second: 2, callback: sleepFunc);
+
+  // Buoi 4 - Bai 2
+  print('\n-------------------Buoi 4 - Bai 2-----------------------');
+  String sampleTextInput = "sampleTextInput";
+  textField(sampleTextInput, onChanged: (value) {
+    print(value);
+  });
+
+  // Buoi 4 - Bai 4.
+
+  print('\n-------------------Buoi 4 - Bai 4-----------------------');
   List<int> randomNumbers = List.generate(30, (index) {
     final random = Random();
     return random.nextInt(100);
   });
 
-  // Print the list of random numbers.
   print('Original numbers           : $randomNumbers');
-
-  // Try to swap the first two elements of the list.
   try {
-    print('Sorted numbers (ascending) : ${sortListInt(targetList: randomNumbers, ascending: true)}');
-    print('Sorted numbers (descending): ${sortListInt(targetList: randomNumbers, ascending: false)}');
+    print(
+        'Sorted numbers (ascending) : ${sortListInt(targetList: randomNumbers, ascending: true)}');
+    print(
+        'Sorted numbers (descending): ${sortListInt(targetList: randomNumbers, ascending: false)}');
   } catch (e) {
     print(e);
   }
-
-  // Print the list of random numbers after the swap.
-  // print('Swapped numbers : $randomNumbers');
-}
-
-///Sort list of int
-List<int> sortListInt(
-    {required List<int> targetList, required bool ascending}) {
-  if (targetList.isEmpty) {
-    throw (Exception("--------------------> Something wrong!!!"));
-  }
-
-  for (var maxLenght = targetList.length; maxLenght > 0; maxLenght--) {
-    for (var j = 0; j < maxLenght - 1; j++) {
-      if (ascending == true
-          ? targetList[j] > targetList[j + 1]
-          : targetList[j] < targetList[j + 1]) {
-        targetList = swapTwoElement(targetList, j, j + 1);
-      }
-      // print(targetList);
-    }
-  }
-
-  return targetList;
-}
-
-List<int> swapTwoElement(
-    List<int> targetList, int firstIndex, int secondIndex) {
-  // Check that the indexes are valid.
-  if (targetList.isEmpty ||
-      firstIndex == secondIndex ||
-      firstIndex > targetList.length - 1 ||
-      secondIndex > targetList.length - 1 ||
-      firstIndex < 0 ||
-      secondIndex < 0) {
-    throw (Exception("--------------------> Something wrong!!!"));
-  }
-
-  // Swap the elements at the specified indexes.
-  int temp = targetList[firstIndex];
-  targetList[firstIndex] = targetList[secondIndex];
-  targetList[secondIndex] = temp;
-
-  return targetList;
 }
---
+++ repo.dart
@@ -0,0 +1,70 @@
+import 'dart:io';
+
+///Buoi 4 - Bai 1
+timerPeriodic({required int second, required Function(int) callback}) {
+  callback(second);
+}
+
+void sleepFunc(int second) {
+  int count = 0;
+  while (count <= second) {
+    print('Current timer value: $count (s)');
+    sleep(Duration(seconds: 1));
+    count++;
+  }
+}
+
+///Buoi 4 - Bai 2
+
+textField(String text, {Function(String)? onChanged}) {
+  if (onChanged != null) {
+    final result = 'Hello $text';
+    onChanged(result);
+  }
+}
+
+String onChangedCallbackFunc(String text) {
+  return text;
+}
+
+///Sort list of int
+List<int> sortListInt(
+    {required List<int> targetList, required bool ascending}) {
+  if (targetList.isEmpty) {
+    throw (Exception("--------------------> Something wrong!!!"));
+  }
+
+  for (var maxLenght = targetList.length; maxLenght > 0; maxLenght--) {
+    for (var j = 0; j < maxLenght - 1; j++) {
+      if (ascending == true
+          ? targetList[j] > targetList[j + 1]
+          : targetList[j] < targetList[j + 1]) {
+        targetList = swapTwoElement(targetList, j, j + 1);
+      }
+      // print(targetList);
+    }
+  }
+
+  return targetList;
+}
+
+///swapTwoElement using index number
+List<int> swapTwoElement(
+    List<int> targetList, int firstIndex, int secondIndex) {
+  // Check that the indexes are valid.
+  if (targetList.isEmpty ||
+      firstIndex == secondIndex ||
+      firstIndex > targetList.length - 1 ||
+      secondIndex > targetList.length - 1 ||
+      firstIndex < 0 ||
+      secondIndex < 0) {
+    throw (Exception("--------------------> Something wrong!!!"));
+  }
+
+  // Swap the elements at the specified indexes.
+  int temp = targetList[firstIndex];
+  targetList[firstIndex] = targetList[secondIndex];
+  targetList[secondIndex] = temp;
+
+  return targetList;
+}
