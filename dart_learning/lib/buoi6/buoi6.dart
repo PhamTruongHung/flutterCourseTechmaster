56587e75dc9c74110d844701f7c292a422cbffc5
Hung <trh.bkdn@gmail.com>
2023-05-23 21:35:43 +0700

    minor fix

--- dart_learning/lib/buoi6/buoi6.dart
+++ dart_learning/lib/buoi6/buoi6.dart
@@ -1,8 +1,21 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 void main() {
-  final reactangle = Reactangle(length: 1, height: 2);
+  final rectangle = Rectangle(length: 1, height: 2);
   try {
-    print(reactangle.calculateArea());
+    print(rectangle.calculateArea());
+  } catch (e) {
+    print(e);
+  }
+
+  final circle = Circle(radius: 1);
+  try {
+    print(circle.calculateArea());
+  } catch (e) {
+    print(e);
+  }
+
+  final triangle = Triangle(lenght: 3, height: 3);
+  try {
+    print(triangle.calculateArea());
   } catch (e) {
     print(e);
   }
@@ -12,6 +25,7 @@ abstract class Shape {
   double? length;
   double? height;
   double? radius;
+
   Shape({
     this.length,
     this.height,
@@ -21,33 +35,38 @@ abstract class Shape {
   double calculateArea();
 }
 
-class Reactangle extends Shape {
-  Reactangle({required double height, required double length})
-      : super(height: height, length: length);
+class Rectangle extends Shape {
+  Rectangle({required double length, required double height})
+      : super(length: length, height: height);
+
   @override
   double calculateArea() {
     if (length == null || height == null) {
-      throw ("Can nhap chieu dai va chieu cao!!!");
+      throw Exception('Please provide both length and height!');
     }
     return length! * height!;
   }
 }
 
 class Circle extends Shape {
+  Circle({required double radius}) : super(radius: radius);
+
   @override
   double calculateArea() {
     if (radius == null) {
-      throw ("Can nhap ban kinh!!!");
+      throw Exception('Please provide the radius!');
     }
     return 3.14 * radius! * radius!;
   }
 }
 
 class Triangle extends Shape {
+  Triangle({required double lenght, required double height})
+      : super(height: height, length: lenght);
   @override
   double calculateArea() {
     if (length == null || height == null) {
-      throw ("Can nhap chieu dai canh day va chieu cao!!!");
+      throw Exception('Please provide both base length and height!');
     }
     return 0.5 * length! * height!;
   }
