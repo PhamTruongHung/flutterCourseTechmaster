95921b9b79c108d15a5ec5f20eecc5308456d77f
Hung <trh.bkdn@gmail.com>
2023-06-01 09:53:21 +0700

    Buoi 9 - Bai 1 (ok)

--- flutter_learning/lib/main.dart
+++ flutter_learning/lib/main.dart
@@ -1,4 +1,3 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 
 void main() {
@@ -34,22 +33,25 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    final statistic = [Keno('Chan', 5), Keno('Le', 2), Keno('Hoa CL', 3)];
+    List<Keno> statistic = [Keno('Chan', 5), Keno('Le', 2), Keno('Hoa CL', 3)];
+    statistic.sort();
+    int kenoCountMax = findMaxCount(statistic).count;
+
+    List<Widget> kenoTable = [];
+    for (var keno in statistic) {
+      kenoTable.add(KenoRow(keno: keno, kenoCountMax: kenoCountMax));
+    }
     return Scaffold(
       body: Center(
         child: Container(
-          width: 600,
+          width: 900,
           height: 300,
           padding: const EdgeInsets.all(20),
           decoration:
               BoxDecoration(border: Border.all(color: Colors.red, width: 2)),
           alignment: Alignment.center,
           child: Column(
-            children: [
-              KenoRow(keno: statistic[0]),
-              KenoRow(keno: statistic[1]),
-              KenoRow(keno: statistic[2]),
-            ],
+            children: kenoTable,
           ),
         ),
       ),
@@ -57,38 +59,109 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
+Keno findMaxCount(List<Keno> kenos) {
+  if (kenos.isEmpty) {
+    throw Exception('List is empty');
+  }
+
+  Keno maxKeno = kenos[0]; // Assume the first element has the maximum count
+
+  for (var i = 1; i < kenos.length; i++) {
+    if (kenos[i].count > maxKeno.count) {
+      maxKeno = kenos[i];
+    }
+  }
+
+  return maxKeno;
+}
+
 class KenoRow extends StatelessWidget {
   final Keno keno;
-  const KenoRow({super.key, required this.keno});
+  final int kenoCountMax;
+  const KenoRow({super.key, required this.keno, required this.kenoCountMax});
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Text(keno.text),
-        Expanded(
-          child: Container(
-            height: 5,
-            decoration: const BoxDecoration(
-              color: Colors.red,
-              borderRadius: BorderRadius.all(
-                Radius.circular(3),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                flex: 8,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      flex: 2,
+                      child: Container(
+                        alignment: Alignment.centerLeft,
+                        child: Text(keno.text),
+                      ),
+                    ),
+                    Expanded(
+                        flex: 6,
+                        child: Row(
+                          children: [
+                            Expanded(
+                              flex: keno.count,
+                              child: Container(
+                                alignment: Alignment.centerLeft,
+                                height: 5,
+                                decoration: const BoxDecoration(
+                                  color: Colors.red,
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(3),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              flex: kenoCountMax - keno.count,
+                              child: const SizedBox(),
+                            ),
+                          ],
+                        ))
+                  ],
+                ),
               ),
-            ),
+              Expanded(
+                flex: 2,
+                child: Container(
+                  alignment: Alignment.centerRight,
+                  child: Text('${keno.count} Lan'),
+                ),
+              ),
+            ],
           ),
-        ),
-        Text('${keno.count} Lan'),
-      ],
+          const Divider(
+            thickness: 1,
+            color: Colors.grey,
+          ),
+        ],
+      ),
     );
   }
 }
 
-class Keno {
+class Keno implements Comparable<Keno> {
   final String text;
   final int count;
 
   Keno(this.text, this.count);
+
+  @override
+  int compareTo(Keno other) {
+    if (count > other.count) {
+      return -1;
+    } else if (count < other.count) {
+      return 1;
+    } else {
+      return text.compareTo(other.text);
+    }
+  }
 }
 
 class RowStar extends StatelessWidget {
