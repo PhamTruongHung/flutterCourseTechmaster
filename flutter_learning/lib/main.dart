b4c5a0b5e463c320c62d5c4bcc531a9d695191d5
Hung <trh.bkdn@gmail.com>
2023-06-02 14:35:08 +0700

    Buoi 10 - Bai 1

--- flutter_learning/lib/main.dart
+++ flutter_learning/lib/main.dart
@@ -1,3 +1,8 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first, prefer_const_literals_to_create_immutables
+// ignore_for_file: prefer_const_constructors
+
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -33,25 +38,29 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
+    List<User> users = [];
+    users.add(User(
+        name: 'name 0',
+        avatarUrl:
+            'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1480&q=80',
+        isUpload: true));
+    users.add(User(name: 'name 1', isUpload: true));
+    users.add(User(name: 'name 2', avatarUrl: 'avatarUrl'));
+
+    // debugPrint(users.toString());
+
     return Scaffold(
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Container(
-              width: 900,
-              height: 300,
-              decoration: BoxDecoration(border: Border.all(color: Colors.red)),
-              alignment: Alignment.center,
-              child: const CoVietNamXua(),
+            Avatar(
+              user: users[0],
             ),
-            Container(
-              width: 900,
-              height: 300,
-              decoration: BoxDecoration(border: Border.all(color: Colors.red)),
-              alignment: Alignment.center,
-              child: const CoThuyDien(),
+            Avatar(
+              user: users[1],
+            ),
+            Avatar(
+              user: users[2],
             ),
           ],
         ),
@@ -60,68 +69,103 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-class CoVietNamXua extends StatelessWidget {
-  const CoVietNamXua({super.key});
+class Avatar extends StatelessWidget {
+  final User user;
+  const Avatar({super.key, required this.user});
 
   @override
   Widget build(BuildContext context) {
-    return const Stack(
-      children: [
-        ColorWidget(
-          widgetName: '',
-          widgetColor: Colors.red,
-          widgetSize: Size(300, 200),
-        ),
-        Positioned(
-          // bottom: 0,
-          child: ColorWidget(
-            widgetName: '',
-            widgetColor: Colors.blue,
-            widgetSize: Size(300, 100),
+    return SizedBox(
+      height: 250,
+      // color: Colors.grey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            flex: 9,
+            child: Stack(
+              alignment: Alignment.bottomRight,
+              children: [
+                user.avatarUrl == null
+                    ? NameBuildAvatar(user: user)
+                    : ClipRRect(
+                        borderRadius: BorderRadius.circular(
+                            100), 
+                        child: Image.network(
+                          user.avatarUrl!,
+                          width: 200,
+                          height: 200,
+                          loadingBuilder: (BuildContext context, Widget child,
+                              ImageChunkEvent? loadingProgress) {
+                            if (loadingProgress == null) {
+                              return child;
+                            } else {
+                              return Center(
+                                child: CircularProgressIndicator(),
+                              );
+                            }
+                          },
+                          errorBuilder: (BuildContext context, Object exception,
+                              StackTrace? stackTrace) {
+                            return NameBuildAvatar(user: user);
+                          },
+                        ),
+                      ),
+                Container(
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(25),
+                    color: user.isUpload ? Colors.white : null,
+                  ),
+                  child: Icon(
+                    user.isUpload ? Icons.file_upload_outlined : null,
+                    size: 40,
+                    color: Colors.red,
+                  ),
+                )
+              ],
+            ),
           ),
-        ),
-        Positioned(
-          top: 50,
-          left: 100,
-          child: Icon(
-            Icons.star_rate,
-            size: 100,
-            color: Colors.yellow,
-          ),
-        ),
-      ],
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(2.0),
+              child: Text(user.name),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
 
-class CoThuyDien extends StatelessWidget {
-  const CoThuyDien({
+class NameBuildAvatar extends StatelessWidget {
+  const NameBuildAvatar({
     super.key,
+    required this.user,
   });
 
+  final User user;
+
   @override
   Widget build(BuildContext context) {
+    var generatedColor = Random().nextInt(Colors.primaries.length);
     return Stack(
+      alignment: Alignment.center,
       children: [
-        ColorWidget(
-          widgetName: '',
-          widgetColor: Colors.lightBlue[900]!,
-          widgetSize: const Size(300, 200),
+        Container(
+          width: 200,
+          height: 200,
+          decoration: BoxDecoration(
+              color: Colors.primaries[generatedColor],
+              borderRadius: BorderRadius.circular(100)),
         ),
-        Positioned(
-          left: 100,
-          child: ColorWidget(
-            widgetName: '',
-            widgetColor: Colors.yellow[600]!,
-            widgetSize: const Size(30, 200),
-          ),
-        ),
-        Positioned(
-          top: 85,
-          child: ColorWidget(
-            widgetName: '',
-            widgetColor: Colors.yellow[600]!,
-            widgetSize: const Size(300, 30),
+        Text(
+          user.name.characters.first.toUpperCase(),
+          style: TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
           ),
         ),
       ],
@@ -129,32 +173,17 @@ class CoThuyDien extends StatelessWidget {
   }
 }
 
-class ColorWidget extends StatelessWidget {
-  const ColorWidget(
-      {super.key,
-      required this.widgetName,
-      required this.widgetColor,
-      required this.widgetSize,
-      this.left,
-      this.top,
-      this.right,
-      this.bottom});
-  final String widgetName;
-  final Color widgetColor;
-  final Size widgetSize;
-  final double? left;
-  final double? top;
-  final double? right;
-  final double? bottom;
+class User {
+  final String name;
+  final String? avatarUrl;
+  final bool isUpload;
+  User({
+    required this.name,
+    this.avatarUrl,
+    bool? isUpload,
+  }) : isUpload = isUpload ?? false;
+
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      // decoration: BoxDecoration(border: Border.all(color: Colors.red)),
-      color: widgetColor,
-      width: widgetSize.width,
-      height: widgetSize.height,
-      child: Text(widgetName),
-    );
-  }
+  String toString() =>
+      'User(name: $name, avatarUrl: $avatarUrl, isUpload: $isUpload)';
 }
