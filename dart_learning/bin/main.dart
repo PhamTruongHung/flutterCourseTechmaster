32b5efdc6197a4c30b5d6924250393fe95b4a615
Hung <trh.bkdn@gmail.com>
2023-06-06 20:01:56 +0700

    leetcode buoi 1 - vidu 2

--- dart_learning/bin/main.dart
+++ dart_learning/bin/main.dart
@@ -1,34 +1,21 @@
 void main() async {
   Solution solution = Solution();
 
-  print(solution.fizzBuzz(10000));
+  print(solution.isPerfectSquare(9));
 }
 
 class Solution {
-  List<String> fizzBuzz(int n) {
-    List<String> result = [];
+  bool isPerfectSquare(int num) {
     int index = 1;
-    while (index <= n) {
-      if (divisibleBy3(index) && !divisibleBy5(index)) {
-        result.add("Fizz");
-      } else if (divisibleBy5(index) && !divisibleBy3(index)) {
-        result.add("Buzz");
-      } else if (divisibleBy3(index) && divisibleBy5(index)) {
-        result.add("FizzBuzz");
-      } else {
-        result.add(index.toString());
-      }
-      // print('$index - $result');
+    int currentSquare = 0;
+
+    while (currentSquare <= num) {
+      currentSquare = index * index;
       index++;
+      if (currentSquare == num) {
+        return true;
+      }
     }
-    return result;
+    return false;
   }
-}
-
-bool divisibleBy3(int n) {
-  return n % 3 == 0 ? true : false;
-}
-
-bool divisibleBy5(int n) {
-  return n % 5 == 0 ? true : false;
 }
