fc6ce1798074ce02f225d2cafbaff64819dd4241
Hung <trh.bkdn@gmail.com>
2023-06-06 21:28:02 +0700

    Leetcode buoi 1 - bai 5 - lemonadeChange

--- dart_learning/bin/main.dart
+++ dart_learning/bin/main.dart
@@ -1,19 +1,70 @@
 void main() async {
   Solution solution = Solution();
 
-  for (var i = 0; i < 10; i++) {
-    print(solution.fib(i));
-  }
+  List<int> bills = [5, 5, 5, 10, 20];
+  // List<int> bills = [5, 5, 10, 10, 20];
+
+  print('\n Final result: ${solution.lemonadeChange(bills)}');
 }
 
 class Solution {
-  int fib(int n) {
-    if (n == 0) {
-      return 0;
-    } else if (n == 1) {
-      return 1;
-    } else {
-      return fib(n - 1) + fib(n - 2);
+  bool lemonadeChange(List<int> bills) {
+    int count5 = 0;
+    int count10 = 0;
+    int count20 = 0;
+    int cashBack = 0;
+
+    bool result = true;
+
+    for (var bill in bills) {
+      print('Start bill----');
+      //Thu tien
+      switch (bill) {
+        case 5:
+          count5 = count5 + 1;
+          break;
+        case 10:
+          count10 = count10 + 1;
+          break;
+        case 20:
+          count20 = count20 + 1;
+          break;
+        default:
+      }
+
+      //Tinh tien thoi
+      cashBack = bill - 5;
+
+      print(
+          'Bill $bill -  Start cashBack: $cashBack - 20: $count20 - 10: $count10 - 5: $count5');
+
+      //Thoi tien
+
+      while (cashBack >= 20 && count20 > 0) {
+        cashBack = cashBack - 20;
+        count20 = count20 - 1;
+      }
+
+      while (cashBack >= 10 && count10 > 0) {
+        cashBack = cashBack - 10;
+        count10 = count10 - 1;
+      }
+      while (cashBack >= 5 && count5 > 0) {
+        cashBack = cashBack - 5;
+        count5 = count5 - 1;
+      }
+      print(
+          'Bill $bill -  Current cashBack: $cashBack - 20: $count20 - 10: $count10 - 5: $count5');
+
+      if (count20 >= 0 && count10 >= 0 && count5 >= 0 && cashBack == 0) {
+        print(true);
+        result = result || true;
+      } else {
+        return false;
+      }
+      print('End bill---- \n');
     }
+
+    return true;
   }
 }
