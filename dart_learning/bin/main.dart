f85b571f6032b9836666c7019632b6cb0822d98c
Hung <trh.bkdn@gmail.com>
2023-06-06 20:23:22 +0700

    leetcode 1 - bai 4

--- dart_learning/bin/main.dart
+++ dart_learning/bin/main.dart
@@ -1,24 +1,19 @@
 void main() async {
   Solution solution = Solution();
 
-  print(solution.isPowerOfTwo(1));
+  for (var i = 0; i < 10; i++) {
+    print(solution.fib(i));
+  }
 }
 
 class Solution {
-  bool isPowerOfTwo(int n) {
-    int currentPowerOfTwo = 1;
-
-    if (n == 1) {
-      return true;
+  int fib(int n) {
+    if (n == 0) {
+      return 0;
+    } else if (n == 1) {
+      return 1;
+    } else {
+      return fib(n - 1) + fib(n - 2);
     }
-
-    while (currentPowerOfTwo <= n) {
-      currentPowerOfTwo = currentPowerOfTwo * 2;
-      if (currentPowerOfTwo == n) {
-        return true;
-      }
-    }
-
-    return false;
   }
 }
