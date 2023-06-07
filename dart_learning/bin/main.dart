56a1a3d2ac4192c3b1f9bdc08f7a4edc9f2fbfd9
Hung Pham <trh.bkdn@gmail.com>
2023-06-07 17:06:16 +0700

    leetcode - countDigits

--- dart_learning/bin/main.dart
+++ dart_learning/bin/main.dart
@@ -1,24 +1,23 @@
 void main() async {
   Solution solution = Solution();
 
-  print(solution.isPowerOfFour(5));
+  print(solution.countDigits(121));
 }
 
 class Solution {
-  bool isPowerOfFour(int n) {
-    int currentPowerOfTwo = 1;
+  int countDigits(int n) {
+    // List<String> uniqueList = n.toString().split('').toSet().toList();
+    List<String> uniqueList = n.toString().split('');
+    print(uniqueList);
 
-    if (n == 1) {
-      return true;
-    }
+    int result = 0;
 
-    while (currentPowerOfTwo <= n) {
-      currentPowerOfTwo = currentPowerOfTwo * 4;
-      if (currentPowerOfTwo == n) {
-        return true;
+    for (var digit in uniqueList) {
+      if (n % int.parse(digit) == 0) {
+        result = result + 1;
       }
     }
 
-    return false;
+    return result;
   }
 }
