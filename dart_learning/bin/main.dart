4a055109e40f33f5fd0e987e9414f4d14c5de74f
Hung <trh.bkdn@gmail.com>
2023-06-22 16:36:31 +0700

    memory card - chua on lam

--- dart_learning/bin/main.dart
+++ dart_learning/bin/main.dart
@@ -1,15 +1,32 @@
+import 'dart:math';
+
 void main() async {
   Solution solution = Solution();
 
-  print(solution.lengthOfLastWord("Hello    world"));
+  print(solution.backspaceCompare("Hello world", "safsdf"));
 }
 
 class Solution {
-  int lengthOfLastWord(String s) {
-    String cleanedString = s.trim().replaceAll(RegExp(r'\s+'), ' ');
-
-    List<String> words = cleanedString.split(' ');
-
-    return words.last.length;
+  bool backspaceCompare(String s, String t) {
+      List<String> st1 = [];
+      List<String> st2 = [];
+      
+      for (var i=0; i < max(s.length, t.length); i++){
+          if(s.length>=i+1){
+              if (s[i]=='#' && st1.isNotEmpty){
+                  st1.removeLast();
+              } else if (s[i] != '#'){
+                  st1.add(s[i]);
+              }
+          }
+          if(t.length>=i+1){
+              if (t[i]=='#' && st2.isNotEmpty){
+                  st2.removeLast();
+              } else if (t[i] != '#'){
+                  st2.add(t[i]);
+              }
+          }
+      }
+    return st1.join() == st2.join();
   }
 }
