9e56f7a04070b4ffaf2982a18147dbd40093e280
Hung <trh.bkdn@gmail.com>
2023-07-15 21:52:06 +0700

    dashline

--- flutter_learning/lib/main.dart
+++ flutter_learning/lib/main.dart
@@ -1,13 +1,90 @@
-import 'package:flutter_learning/toast/toast_context.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 import 'package:flutter/material.dart';
 
-GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+void main() {
+  runApp(const MyApp());
+}
 
-void main() => runApp(
-      MaterialApp(
-        builder: FToastBuilder(),
-        home: const ToastContext(),
-        navigatorKey: navigatorKey,
+class DashedLinePainter extends CustomPainter {
+  final Color color;
+  final double strokeWidth;
+  final double dashLength;
+  final double dashSpace;
+
+  DashedLinePainter({
+    required this.color,
+    required this.strokeWidth,
+    required this.dashLength,
+    required this.dashSpace,
+  });
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = color
+      ..strokeWidth = strokeWidth
+      ..style = PaintingStyle.stroke;
+
+    final totalSegments = size.width / (dashLength + dashSpace);
+    final segmentSpace = size.width / totalSegments;
+
+    double startX = 0.0;
+
+    while (startX < size.width) {
+      canvas.drawLine(
+        Offset(startX, 0),
+        Offset(startX + dashLength, 0),
+        paint,
+      );
+
+      startX += segmentSpace;
+    }
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return false;
+  }
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Dashed Line Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.arrow_back_ios_new,
+                color: Colors.black,
+              )),
+          elevation: 0,
+          backgroundColor: Colors.white,
+          title: const Text(
+            'Custom Paint',
+            style: TextStyle(color: Colors.black),
+          ),
+        ),
+        body: CustomPaint(
+          painter: DashedLinePainter(
+            color: Colors.black,
+            strokeWidth: 2.0,
+            dashLength: 8.0,
+            dashSpace: 4.0,
+          ),
+          // ignore: prefer_const_constructors
+          child: SizedBox(
+            width: double.infinity, // Set width to full screen width
+            height: 2.0,
+          ),
+        ),
       ),
     );
+  }
+}
